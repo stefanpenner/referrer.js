e8fd9b12316858823ee5324f080e96c87eca03ea
Stefan Penner <stefan.penner@gmail.com>
2009-09-01 17:49:24 -0500

    added referrer.protocol()

--- referrer.js
+++ referrer.js
@@ -3,12 +3,12 @@ var Iamstef = Iamstef || {};
 Iamstef.referrer = (function(){
 
   //precompile the REGX to  match url : capture keywords
-  var REGEXP   = new RegExp( /^https?:\/\/((?:[\w\-]+\.)+(?:[\w\-]+))\/(?:(?:.+)?[\?&](?:q|p|query|term)=([^&]+)&?)?/i);
+  var REGEXP     = new RegExp( /^(\w+?):\/\/((?:[\w\-]+\.)+(?:[\w\-]+))\/(?:(?:.+)?[\?&](?:q|p|query|term)=([^&]+)&?)?/i);
   // captures [ line,domain,keywords]; 
  
 
   // parse a given url
-  var parse = function(url){
+  var keywords = function(url){
 
     var keywords = '', 
         regx, 
@@ -21,9 +21,9 @@ Iamstef.referrer = (function(){
       var matches = REGEXP.exec(url);
       console.log(matches);
       // matches = [ url,domain,keywords];
-      if(matches.length !== 3) { return ''; }
+      if(matches.length !== 4) { return ''; }
        
-      uncleaned_keywords = matches[2];
+      uncleaned_keywords = matches[3];
       
       keywords = decodeURIComponent(uncleaned_keywords)    
                   .replace(/\s+/g,'+')         // remove whitespace 
@@ -44,25 +44,49 @@ Iamstef.referrer = (function(){
     try{
       matches = REGEXP.exec(url);
       
-      (matches.length < 2) ?  
+      (matches.length < 3) ?  
         hostname =  '' :
-        hostname = matches[1];
+        hostname = matches[2];
     
     }catch (err){ hostname =  '' }
 
     return hostname;
   };
+
+  
+  var protocol = function(url){
+    var matches, protocol;
+    
+    try{
+      matches = REGEXP.exec(url);
+      
+      (matches.length < 2) ?  
+        protocol =  '' :
+        protocol = matches[1];
+    
+    }catch (err){ protocol =  '' }
+
+    return protocol;
+  };
+
   return { 
     version  : "0.0.1",
+    "protocol" : function(){
+      var url = ( arguments[0] !== undefined ) ? arguments[0] : document.referrer;
 
+      return protocol(url);
+    },
     "hostname" : function(){
-      return ( arguments[0] !== undefined ) ? hostname(arguments[0]) : hostname(document.referrer);
+      var url = ( arguments[0] !== undefined ) ? arguments[0] : document.referrer;
+
+      return hostname(url);
     },
     // keywords() will parse the keywords from document.referrer
     // keywords(some_url) will parse the keywords from the given url
     "keywords" : function() { 
-      
-      return ( arguments[0] !== undefined ) ? parse(arguments[0]) : parse(document.referrer); 
+      var url = ( arguments[0] !== undefined ) ? arguments[0] : document.referrer;
+
+      return keywords(url);
     }};
 })();
 
