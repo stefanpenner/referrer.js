2f445590000f2377b8b738ea24bd40ea22edf59e
Stefan Penner <stefan.penner@gmail.com>
2009-09-01 17:39:50 -0500

    added referrer.hostname(), tests pending

--- referrer.js
+++ referrer.js
@@ -3,7 +3,7 @@ var Iamstef = Iamstef || {};
 Iamstef.referrer = (function(){
 
   //precompile the REGX to  match url : capture keywords
-  var REGEXP = new RegExp( /^https?:\/\/((?:[\w\-]+\.)+(?:[\w\-]+))\/(?:.+)?[\?&](?:q|p|query|term)=([^&]+)&?/i);
+  var REGEXP   = new RegExp( /^https?:\/\/((?:[\w\-]+\.)+(?:[\w\-]+))\/(?:(?:.+)?[\?&](?:q|p|query|term)=([^&]+)&?)?/i);
   // captures [ line,domain,keywords]; 
  
 
@@ -19,29 +19,48 @@ Iamstef.referrer = (function(){
     try{
 
       var matches = REGEXP.exec(url);
+      console.log(matches);
       // matches = [ url,domain,keywords];
       if(matches.length !== 3) { return ''; }
-      
-      console.log(matches);
+       
       uncleaned_keywords = matches[2];
-
+      
       keywords = decodeURIComponent(uncleaned_keywords)    
                   .replace(/\s+/g,'+')         // remove whitespace 
                   .replace(/\+\++/g,'+')       // collapse consecutive '+'
                   .replace(/[\+\.]+$/,'')      // remove whitespace 
                   .replace(/^\++/,'');         // remove whitespace 
+      
+      if(keywords === "undefined") { keywords = ''; }
 
     }catch (err){ keywords = ''; }
 
     return keywords;
   };
 
+  var hostname = function(url){
+    var matches, hostname;
+    
+    try{
+      matches = REGEXP.exec(url);
+      
+      (matches.length < 2) ?  
+        hostname =  '' :
+        hostname = matches[1];
+    
+    }catch (err){ hostname =  '' }
+
+    return hostname;
+  };
   return { 
     version  : "0.0.1",
-             
+
+    "hostname" : function(){
+      return ( arguments[0] !== undefined ) ? hostname(arguments[0]) : hostname(document.referrer);
+    },
     // keywords() will parse the keywords from document.referrer
     // keywords(some_url) will parse the keywords from the given url
-    keywords : function() { 
+    "keywords" : function() { 
       
       return ( arguments[0] !== undefined ) ? parse(arguments[0]) : parse(document.referrer); 
     }};
